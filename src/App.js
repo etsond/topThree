8e93668e1576d913731d33298e76d3b010968756
Etson <etsond@ufl.edu>
2023-01-06 10:50:49 -0500

    updated styles, info

--- src/App.js
+++ src/App.js
@@ -1,33 +1,69 @@
 
 import 'bulma/css/bulma.css';
 import ProfileCard from "./ProfileCard";
-import AlexaImage from './images/alexa.png';
-import CortanaImage from './images/cortana.png';
-import SiriImage from './images/siri.png';
+import Travelers from './images/travelers.jpeg';
+import HomeLand from './images/homeland.png';
+import SeinFeld from './images/seinfeld.png';
 
 
 function App(){
   return (
     <div>
-       <div>Personal Digital Assistan</div>
+    {/* header */}
+       <section className='hero is-info'>
+        <div className='hero-body'>
+          <p className='title'> My Top Three Favorite Shows.</p>
+        </div>
+       </section>
 
       <div className='container'>
         <section className='section'>
           <div className='columns'>
             <div className='column is-3'>
-  <ProfileCard title="Alexa" handle="@alexa99" image = {AlexaImage} />
+                      <ProfileCard 
+                      title="Travelers" 
+                      handle="@TRVLRSseries" 
+                      image = {Travelers} 
+                      description="
+                      Hundreds of years from now, 
+                      surviving humans discover 
+                      how to send consciousness back 
+                      through time, into people of the 
+                      21st century, while attempting to 
+                      change the path of humanity." />
 
             </div>
             <div className='column is-3'>
-                    <ProfileCard title="Cortana" handle="@Cortana32" image ={CortanaImage} />
-
-
+                    <ProfileCard 
+                    title="HomeLand" 
+                    handle="@SHO_Homeland" 
+                    image ={HomeLand} 
+                    description="
+                    A CIA operative 
+                    becomes convinced a prisoner
+                     of war has been turned by 
+                     a terror group and is planning to
+                      carry out an attack
+                       on American soil. The show showcased 
+                       different topics around her life,  
+                        from good and bad."
+                    />
           </div>
           <div className='column is-3'>
-
-      <ProfileCard title="Siri" handle="@Siri01" image={SiriImage}/>
-
-
+                    <ProfileCard 
+                    title="SeinFeld" 
+                    handle="@SeinfeldTV" 
+                    image={SeinFeld}
+                    description="
+                     Seinfeld as a stand-up 
+                     comedian whose life in 
+                     New York City is made even 
+                     more chaotic by his quirky 
+                     group of friends who join him 
+                     in wrestling with life's most 
+                     perplexing, yet often trivial questions.
+                     such as Airplane Peanuts?"
+                    />
           </div>
         </div>
         </section>
